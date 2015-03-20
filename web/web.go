f3ebeff1493a0f8dfbc79cb26186bb06e48ee417
Steven Lu <tacticalazn@gmail.com>
2015-03-19 23:36:28 -0400

    more structuring and signup

--- web/web.go
+++ web/web.go
@@ -1,29 +1,16 @@
 package web
 
 import (
-	"appengine"
-	"appengine/user"
+	// "appengine"
+	// "appengine/user"
 	"fmt"
 	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/api", newHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
-	if u == nil {
-		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
-		return
-	}
-
-	fmt.Fprint(w, "Hello, %v!", u)
+func newHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Rawr")
 }
